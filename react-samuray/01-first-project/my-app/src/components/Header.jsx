c56d56030ae30c36f486a5f511c32c0f67d1b0e6
Vladyslav <kupriianovvladyslav@gmail.com>
2021-07-18 15:45:30 +0300

    first react commit

---
+++ react-samuray/01-first-project/my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.kindpng.com/picc/m/116-1167355_copyright-symbol-transparent-background-png-small-transparent-background.png'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ react-samuray/01-first-project/my-app/src/components/Profile.jsx
@@ -0,0 +1,29 @@
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div>
+                <img src='https://miro.medium.com/max/1124/1*92adf06PCF91kCYu1nPLQg.jpeg'/>
+            </div>
+            <div>
+                avatar + description
+                {/*<img src='https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'/>*/}
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
